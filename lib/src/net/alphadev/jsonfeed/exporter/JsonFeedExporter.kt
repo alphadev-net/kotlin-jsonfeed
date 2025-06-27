901ac66d7dfe9fb5b5d2efd8b1c23a61a2c076c1
Jan Seeger <jan@alphadev.net>
2025-06-27 19:17:52 +0200

    Fix Json instance reuse warning

--- lib/src/net/alphadev/jsonfeed/exporter/JsonFeedExporter.kt
+++ lib/src/net/alphadev/jsonfeed/exporter/JsonFeedExporter.kt
@@ -3,12 +3,8 @@ package net.alphadev.jsonfeed.exporter
 import net.alphadev.jsonfeed.format.JsonFeed
 import kotlinx.serialization.json.Json
 
-private const val INDENT_SIZE = 4
-
-private val json = Json {}
-
 fun formatJsonFeed(jsonFeed: JsonFeed): String? = try {
-    json.encodeToString(JsonFeed.serializer(), jsonFeed) + "\n"
+    Json.encodeToString(JsonFeed.serializer(), jsonFeed) + "\n"
 } catch (_: Throwable) {
     null
 }
--- lib/src/net/alphadev/jsonfeed/import/JsonFeedParser.kt
+++ lib/src/net/alphadev/jsonfeed/import/JsonFeedParser.kt
@@ -1,13 +1,10 @@
 package net.alphadev.jsonfeed.import
 
-import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import net.alphadev.jsonfeed.format.JsonFeed
 
-private val json = Json {}
-
 fun parseJsonFeed(input: String): JsonFeed? = try {
-    json.decodeFromString<JsonFeed>(input)
+    Json.decodeFromString<JsonFeed>(input)
 } catch (_: Throwable) {
     null
 }
