0f0436ce905643be2cdb7d10b49a6490ef02f89d
Jan Seeger <jan@alphadev.net>
2025-07-27 21:50:30 +0200

    Introduce FeedParsingException

---
+++ lib/src/net/alphadev/jsonfeed/import/FeedParsingException.kt
@@ -0,0 +1,5 @@
+package net.alphadev.jsonfeed.import
+
+import java.lang.Exception
+
+class FeedParsingException(message: String? = null, cause: Throwable? = null): Exception(message, cause)
--- lib/src/net/alphadev/jsonfeed/import/JsonFeedParser.kt
+++ lib/src/net/alphadev/jsonfeed/import/JsonFeedParser.kt
@@ -3,8 +3,8 @@ package net.alphadev.jsonfeed.import
 import kotlinx.serialization.json.Json
 import net.alphadev.jsonfeed.format.JsonFeed
 
-fun parseJsonFeed(input: String): JsonFeed? = try {
+fun parseJsonFeed(input: String): JsonFeed = try {
     Json.decodeFromString<JsonFeed>(input)
-} catch (_: Throwable) {
-    null
+} catch (ex: Throwable) {
+    throw FeedParsingException(cause = ex)
 }
