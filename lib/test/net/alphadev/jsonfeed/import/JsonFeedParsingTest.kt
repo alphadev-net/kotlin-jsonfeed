a4dc93b27be31b068f6a8d60db2348a1461315dd
Jan Seeger <jan@alphadev.net>
2025-10-15 00:52:55 +0200

    Remove unused Opt-in, is already defined in Amper module

--- lib/test/net/alphadev/jsonfeed/import/JsonFeedParsingTest.kt
+++ lib/test/net/alphadev/jsonfeed/import/JsonFeedParsingTest.kt
@@ -8,10 +8,8 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
 import kotlin.test.assertIs
-import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
-@OptIn(ExperimentalTime::class)
 class JsonFeedParsingTest {
 
     @Test
